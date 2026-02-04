64d49443d9b41496d126f687843c96b38594f75f
Diego Cárcamo <39307881+diegoaxity@users.noreply.github.com>
2026-02-04 15:58:40 -0600

    Update iOS platform version and dependency version

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorLottieSplashScreen",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorLottieSplashScreenPlugin",
             targets: ["LottieSplashScreenPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.3.0")
     ],
     targets: [
